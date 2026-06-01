f384ae23fcd5b8cc9a4f32388bd009ae6c37f4b6
Mac-Anu <mac-anu@users.noreply.github.com>
2026-06-01 19:03:18 +0800

    fix: perfectly align drag handle for indented blocks (lists)

--- src/components/article/editor/hooks/useEditorSetup.ts
+++ src/components/article/editor/hooks/useEditorSetup.ts
@@ -189,7 +189,7 @@ export function useEditorSetup({
             TableCell,
             GlobalDragHandle.configure({
                 dragHandleSelector: ".custom-drag-handle",
-                dragHandleWidth: 72,
+                dragHandleWidth: 24,
                 scrollTreshold: 100,
             }),
             AutoJoiner,
